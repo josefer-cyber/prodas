c7cfe12aacdcf6c77e6b3c278b103cbf8af7cb7c
josefer-cyber <j371185@gmail.com>
2025-07-31 15:26:52 +0700

    Add files via upload

--- ajn.cpp
+++ ajn.cpp
@@ -1,13 +1,48 @@
-#include <iostream>
+#include <iostream> 
 using namespace std;
+
 int main() {
-    int time = 22;
-    if (time < 10 {
-        cout << "Good Morning.";
-    } else if (time < 20) {
-        cout << "Good day.";
-    } else {
-        cout << "Good evening.";
-    }
-    return 0;
-}
+
+// int x = 20;
+
+// int y = 18;
+
+// if (x > y) {
+
+// cout << "x is greater than y";
+
+// }
+
+// return 0;
+
+
+//     int time = 20;
+//     if (time < 18) {
+//         cout << "Good day.";
+//     }
+//     else {
+//         cout << "Good evening.";
+//     }
+//     return 0;
+// }
+
+
+    // int time = ;
+    // if (time < 18) {
+    //     cout << "Good day." << endl;
+    // }   else {
+    //     cout << "Good evening." << endl;
+    // }
+    // return 0;
+
+
+// int time = 22;
+//     if (time < 10) {
+//         cout << "Good morning." << endl;
+//     }   else if (time < 20) {
+//         cout << "Good day." << endl;
+//     } else {
+//         cout << "Good evening." << endl;
+//     }   
+//     return 0;
+// }
